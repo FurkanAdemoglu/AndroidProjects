25c5620017bc294699775ecb51d5a2e8c8ff31c1
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-06 12:59:25 +0300

    Async await yapısı kodlandı

--- AndroidCoroutinesPartTwo/app/src/main/java/com/example/androidcoroutinesparttwo/MainActivity.kt
+++ AndroidCoroutinesPartTwo/app/src/main/java/com/example/androidcoroutinesparttwo/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.coroutines.*
+import kotlin.system.measureNanoTime
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val job=GlobalScope.launch(Dispatchers.Default) {
+
+        //5. Lesson
+       /* val job=GlobalScope.launch(Dispatchers.Default) {
           /*  repeat(5){
                 Log.d(TAG,"Coroutine is still working...")
                 delay(1000L)
@@ -30,7 +34,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             Log.d(TAG,"Ending long running calculation...")
-        }
+        }*/
 
       /*  runBlocking {
             delay(2000L)
@@ -39,11 +43,34 @@ class MainActivity : AppCompatActivity() {
             //Log.d(TAG,"Main Thread is continuing...")
             Log.d(TAG,"Canceled job!")
         }*/
-    }
 
-    fun fib(n:Int):Long{
+        GlobalScope.launch(Dispatchers.IO) {
+            val time= measureTimeMillis {
+
+                //BU kurulan yapıda yaklaşık 9000ms sürüyo bu da verimli bir yol değil onun için async
+                //await yapısı kullanacağız
+
+                //val answer1=networkCall1()
+                //val answer2=networkCall2()
+                val answer1=async { networkCall1() }
+                val answer2=async { networkCall2() }
+                Log.d(TAG,"Answer1 is ${answer1.await()}")
+                Log.d(TAG,"Answer2 is ${answer2.await()}")
+            }
+            Log.d(TAG,"Requests took $time ms.")
+        }
+    }
+suspend fun networkCall1():String{
+    delay(3000L)
+    return "Answer 1"
+}
+    suspend fun networkCall2():String{
+        delay(3000L)
+        return "Answer 2"
+    }
+   /* fun fib(n:Int):Long{
         return if(n==0) 0
         else if(n==1) 1
         else fib(n-1)+fib(n-2)
-    }
+    }*/
 }
