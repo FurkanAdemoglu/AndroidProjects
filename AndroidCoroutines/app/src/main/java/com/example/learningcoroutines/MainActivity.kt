30c332a329709612e238983dd3781c751891664c
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-06 10:09:28 +0300

    Coroutine contexts dispatchers type

--- AndroidCoroutines/app/src/main/java/com/example/learningcoroutines/MainActivity.kt
+++ AndroidCoroutines/app/src/main/java/com/example/learningcoroutines/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.learningcoroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         //Burada uygulama ekrana geldikten sonra eğer uygulamayı kapatırsak coroutine
         //içindeki log kısmı da gelmez çünkü main thread de bitmiştir.
         //İlk ders kodları
-       /* GlobalScope.launch {
+        /* GlobalScope.launch {
             delay(3000L)
             Log.d(TAG,"Coroutine says hello from thread ${Thread.currentThread().name}")
         }
@@ -28,21 +26,46 @@ class MainActivity : AppCompatActivity() {
         //Suspend fonksiyonlar mevcut threadi bloke etmeden coroutinin yürütülmesini
         //askıya alır. Böylelikle thread başka bir coroutinen işletilmesine başlar ve
         //cpu daha verimli kullanılır
-        GlobalScope.launch {
+        //2. ders kodları
+        /*GlobalScope.launch {
             val networkCallAnswer=doNetworkCall()
             val networkCallAnswer2=doNetworkCall2()
             Log.d(TAG,networkCallAnswer)
             Log.d(TAG,networkCallAnswer2)
+        }*/
+
+
+        //3. ders kodları
+        //Dispatcher.Main => Coroutine i main threadinde başlatır. UI operasyonları coroutinede yapmak
+        //istediğimiz zaman kullanabiliriz.Çünkü UI ı sadece main threadinden değiştirebiliriz
+
+        //Dispatcher.IO => Data operasyonlarında kullanılır(Ör: API den veri çekme database e veri yazma )
+        //Dispatcher.Default=> Uzun hesaplama sıralama işlemlerinde kullanılır
+        //Dispatcher.Unconfined
+        GlobalScope.launch(Dispatchers.IO) {
+
+            //Network den istek alığımız gibi düşündüğümüz için bu dispatcher ı kullandık
+        Log.d(TAG,"Starting coroutine in the thread ${Thread.currentThread().name}")
+            val answer=doNetworkCall()
+            withContext(Dispatchers.Main){
+                //Burada UI işlemlerı yaptığımız için withContext ile main thread e geçtik
+                Log.d(TAG,"Setting text in thread ${Thread.currentThread().name}")
+                //textDummy.text=answer
+            }
         }
+
     }
 
+    //2. ders kodları
     suspend fun doNetworkCall():String{
         delay(3000L)
         return "This is the answer"
     }
 
-    suspend fun doNetworkCall2():String{
+   /* suspend fun doNetworkCall2():String{
         delay(3000L)
         return "This is the answer"
-    }
+    }*/
+
+
 }
