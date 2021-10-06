af34ace638edea7277ee4106db8800430cdfd352
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-06 09:48:20 +0300

    Lesson Two Coroutines suspend functions

--- AndroidCoroutines/app/src/main/java/com/example/learningcoroutines/MainActivity.kt
+++ AndroidCoroutines/app/src/main/java/com/example/learningcoroutines/MainActivity.kt
@@ -13,11 +13,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        GlobalScope.launch {
+
+        //Burada uygulama ekrana geldikten sonra eğer uygulamayı kapatırsak coroutine
+        //içindeki log kısmı da gelmez çünkü main thread de bitmiştir.
+        //İlk ders kodları
+       /* GlobalScope.launch {
             delay(3000L)
             Log.d(TAG,"Coroutine says hello from thread ${Thread.currentThread().name}")
         }
 
-        Log.d(TAG,"Hello from thread ${Thread.currentThread().name}")
+        Log.d(TAG,"Hello from thread ${Thread.currentThread().name}")*/
+        //Bu dersteki öenmli nokta suspend fonksiyonlar sadece coroutineler içinden
+        //ya da başka bir suspend fonksiyon içinden çağıralabilirler
+        //Suspend fonksiyonlar mevcut threadi bloke etmeden coroutinin yürütülmesini
+        //askıya alır. Böylelikle thread başka bir coroutinen işletilmesine başlar ve
+        //cpu daha verimli kullanılır
+        GlobalScope.launch {
+            val networkCallAnswer=doNetworkCall()
+            val networkCallAnswer2=doNetworkCall2()
+            Log.d(TAG,networkCallAnswer)
+            Log.d(TAG,networkCallAnswer2)
+        }
+    }
+
+    suspend fun doNetworkCall():String{
+        delay(3000L)
+        return "This is the answer"
+    }
+
+    suspend fun doNetworkCall2():String{
+        delay(3000L)
+        return "This is the answer"
     }
 }
