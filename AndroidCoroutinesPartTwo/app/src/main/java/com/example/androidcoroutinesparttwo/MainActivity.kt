a6e66e42e548b7e8f93355e87adce2b3311e04ce
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-06 12:26:24 +0300

    Coroutines part two created and coded for fifth lesson

---
+++ AndroidCoroutinesPartTwo/app/src/main/java/com/example/androidcoroutinesparttwo/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.androidcoroutinesparttwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG="MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val job=GlobalScope.launch(Dispatchers.Default) {
+          /*  repeat(5){
+                Log.d(TAG,"Coroutine is still working...")
+                delay(1000L)
+            }*/
+
+            Log.d(TAG,"Starting long running calculation....")
+
+            withTimeout(3000L){//=>withTimeout 3 saniye sürelğine çalışacak bu yüzden run blocking kısmını oddan kaldırdık
+
+                for(i in 30..40){
+                    if(isActive){
+                        Log.d(TAG,"Result for i = $i : ${fib(i)}")
+                    }
+
+                }
+            }
+            Log.d(TAG,"Ending long running calculation...")
+        }
+
+      /*  runBlocking {
+            delay(2000L)
+            //job.join()
+            job.cancel()
+            //Log.d(TAG,"Main Thread is continuing...")
+            Log.d(TAG,"Canceled job!")
+        }*/
+    }
+
+    fun fib(n:Int):Long{
+        return if(n==0) 0
+        else if(n==1) 1
+        else fib(n-1)+fib(n-2)
+    }
+}
