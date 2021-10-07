74c7144889d6e986c778374c8f0399e9fee2c954
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-07 12:25:41 +0300

    Bind and Provide annotations in dagger hilt

--- DaggerHiltInjectionsPartTwo/app/src/main/java/com/example/daggerhiltinjectionsparttwo/MainActivity.kt
+++ DaggerHiltInjectionsPartTwo/app/src/main/java/com/example/daggerhiltinjectionsparttwo/MainActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Singleton
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -24,24 +32,68 @@ class MainActivity : AppCompatActivity() {
 class SomeClass
 @Inject
 constructor(
-    //private val someInterfaceImpl: SomeInterface,
-    private val gson:Gson
+    private val someInterfaceImpl: SomeInterface,
+    private val gson: Gson
 ){
     fun doAThing(): String{
-       // return "Look I got: ${someInterfaceImpl.getAThing()}"
-        return "somethng"
+        return "Look I got: ${someInterfaceImpl.getAThing()}"
+
     }
 }
 
 class SomeInterfaceImpl
 @Inject
-constructor():SomeInterface{
+constructor(
+    private val someDependency:String
+):SomeInterface{
     override fun getAThing(): String {
-        return "A thing"
+        return "A thing, ${someDependency}"
     }
 
 }
 
 interface SomeInterface{
     fun getAThing():String
+}
+
+/*@InstallIn(ActivityComponent::class)
+@Module
+abstract class MyModule{
+
+    @ActivityScoped
+    @Binds
+    abstract fun bindSomeDependency(
+        someImpl:SomeInterfaceImpl
+    ):SomeInterface
+
+    @ActivityScoped
+    @Binds
+    abstract fun bindGson(
+        gson: Gson
+    ):Gson
+}*/
+
+@InstallIn(ApplicationComponentManager::class)
+@Module
+class MyModule{
+
+    @Singleton
+    @Provides
+    fun provideSomeString():String{
+        return "some string"
+    }
+    @Singleton
+    @Provides
+    fun provideSomeInterface(
+        someString: String
+    ):SomeInterface{
+        return SomeInterfaceImpl(someString)
+    }
+
+    @Singleton
+    @Provides
+    fun provideGson():Gson{
+        return Gson()
+    }
+
 }
