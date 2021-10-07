5ca22a253536327e59024ac42518784ed97c728d
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-07 12:58:46 +0300

    Injection problems solving with qualifier annotations

--- DaggerHiltInjectionsPartTwo/app/src/main/java/com/example/daggerhiltinjectionsparttwo/MainActivity.kt
+++ DaggerHiltInjectionsPartTwo/app/src/main/java/com/example/daggerhiltinjectionsparttwo/MainActivity.kt
@@ -13,6 +13,7 @@ import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.internal.managers.ApplicationComponentManager
 import dagger.hilt.android.scopes.ActivityScoped
 import javax.inject.Inject
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @AndroidEntryPoint
@@ -25,29 +26,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println(someClass.doAThing())
+        println(someClass.doAThing1())
+        println(someClass.doAThing2())
     }
 }
 
 class SomeClass
 @Inject
 constructor(
-    private val someInterfaceImpl: SomeInterface,
-    private val gson: Gson
+    @Impl1 private val someInterfaceImpl1: SomeInterface,
+    @Impl2 private val someInterfaceImpl2: SomeInterface
 ){
-    fun doAThing(): String{
-        return "Look I got: ${someInterfaceImpl.getAThing()}"
+    fun doAThing1(): String{
+        return "Look I got: ${someInterfaceImpl1.getAThing()}"
+
+    }
+
+    fun doAThing2(): String{
+        return "Look I got: ${someInterfaceImpl2.getAThing()}"
 
     }
 }
 
-class SomeInterfaceImpl
+class SomeInterfaceImpl1
 @Inject
 constructor(
-    private val someDependency:String
+   // private val someDependency:String
 ):SomeInterface{
     override fun getAThing(): String {
-        return "A thing, ${someDependency}"
+        return "A thing1"//, ${someDependency}"
+    }
+
+}
+
+class SomeInterfaceImpl2
+@Inject
+constructor(
+    // private val someDependency:String
+):SomeInterface{
+    override fun getAThing(): String {
+        return "A thing2"//, ${someDependency}"
     }
 
 }
@@ -77,23 +95,41 @@ abstract class MyModule{
 @Module
 class MyModule{
 
-    @Singleton
+    /*@Singleton
     @Provides
     fun provideSomeString():String{
         return "some string"
-    }
+    }*/
+    @Impl1
     @Singleton
     @Provides
-    fun provideSomeInterface(
+    fun provideSomeInterface1(
         someString: String
     ):SomeInterface{
-        return SomeInterfaceImpl(someString)
+        return SomeInterfaceImpl1()
+    }
+@Impl2
+    @Singleton
+    @Provides
+    fun provideSomeInterface2(
+        someString: String
+    ):SomeInterface{
+        return SomeInterfaceImpl2()
     }
 
-    @Singleton
+    /*@Singleton
     @Provides
     fun provideGson():Gson{
         return Gson()
-    }
+    }*/
 
 }
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Impl1
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class Impl2
+
