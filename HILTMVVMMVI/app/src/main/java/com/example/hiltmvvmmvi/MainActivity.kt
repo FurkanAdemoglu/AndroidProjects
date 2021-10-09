c254b463226af4353d9018ebdefbfafc6f8dd19b
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-09 12:17:58 +0300

    Application file created and modules handdled

--- HILTMVVMMVI/app/src/main/java/com/example/hiltmvvmmvi/MainActivity.kt
+++ HILTMVVMMVI/app/src/main/java/com/example/hiltmvvmmvi/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.hiltmvvmmvi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val TAG:String="AppDebug"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ HILTMVVMMVI/app/src/main/java/com/example/hiltmvvmmvi/repository/MainRepository.kt
@@ -0,0 +1,4 @@
+package com.example.hiltmvvmmvi.repository
+
+class MainRepository {
+}
