28cc3ff884a2d45173fe931fd1d511577370a525
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-09 13:13:34 +0300

    MainViewModel created and coded

--- HILTMVVMMVI/app/src/main/java/com/example/hiltmvvmmvi/di/RepositoryModule.kt
+++ HILTMVVMMVI/app/src/main/java/com/example/hiltmvvmmvi/di/RepositoryModule.kt
@@ -1,4 +1,28 @@
 package com.example.hiltmvvmmvi.di
 
+import com.example.hiltmvvmmvi.repository.MainRepository
+import com.example.hiltmvvmmvi.retrofit.BlogRetrofit
+import com.example.hiltmvvmmvi.retrofit.NetworkMapper
+import com.example.hiltmvvmmvi.room.BlogDao
+import com.example.hiltmvvmmvi.room.CacheMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
 object RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideMainRepository(
+        blogDao: BlogDao,
+        retrofit: BlogRetrofit,
+        cacheMapper: CacheMapper,
+        networkMapper: NetworkMapper
+    ):MainRepository{
+        return MainRepository(blogDao,retrofit,cacheMapper,networkMapper)
+    }
 }
--- HILTMVVMMVI/app/src/main/java/com/example/hiltmvvmmvi/ui/MainViewModel.kt
+++ HILTMVVMMVI/app/src/main/java/com/example/hiltmvvmmvi/ui/MainViewModel.kt
@@ -2,10 +2,16 @@ package com.example.hiltmvvmmvi.ui
 
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import com.example.hiltmvvmmvi.model.Blog
 import com.example.hiltmvvmmvi.repository.MainRepository
+import com.example.hiltmvvmmvi.util.DataState
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 
+@ExperimentalCoroutinesApi
 class MainViewModel
 @ViewModelInject constructor(
     private val mainRepository: MainRepository,
@@ -13,4 +19,31 @@ class MainViewModel
 
 ):ViewModel(){
 
+    private val _dataState:MutableLiveData<DataState<List<Blog>>> = MutableLiveData()
+
+    val dataState:LiveData<DataState<List<Blog>>>
+    get() = _dataState
+
+    fun setStateEvent(mainStateEvent: MainStateEvent){
+        viewModelScope.launch {
+            when(mainStateEvent){
+                is MainStateEvent.GetBlogEvents->{
+                    mainRepository.getBlog()
+                        .onEach {dataState ->
+                        _dataState.value=dataState
+                        }
+                        .launchIn(viewModelScope)
+                }
+                is MainStateEvent.None->{
+
+                }
+            }
+        }
+    }
+}
+
+sealed class MainStateEvent{
+    object GetBlogEvents:MainStateEvent()
+
+    object None:MainStateEvent()
 }
