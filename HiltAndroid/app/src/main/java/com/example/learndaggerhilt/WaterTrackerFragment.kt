de6f79e8415f564c3e5665ed1d9065d946f4de63
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-10-17 12:54:52 +0300

    Viewmodel injections

--- HiltAndroid/app/src/main/java/com/example/learndaggerhilt/WaterTrackerFragment.kt
+++ HiltAndroid/app/src/main/java/com/example/learndaggerhilt/WaterTrackerFragment.kt
@@ -5,14 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class WaterTrackerFragment: Fragment(),WaterintakePreferencesListener {
+class WaterTrackerFragment: Fragment() {
 
-    @Inject
-    lateinit var preferencesHelper: PreferencesHelper
+    private val viewModel:WaterTrackingViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,15 +30,14 @@ class WaterTrackerFragment: Fragment(),WaterintakePreferencesListener {
         /*trackWaterButton.setOnClickListener{
             preferencesHelper.incrementWaterIntake()
         }*/
-        preferencesHelper.subscribeToWaterIntakeChanges(this)
-    }
+       // preferencesHelper.subscribeToWaterIntakeChanges(this)
+        viewModel.liveData.observe(viewLifecycleOwner, Observer {
+            //waterCountText.text=it.toString()
+        })
 
-    override fun onDestroyView() {
-        preferencesHelper.unsubscribeFromWaterIntakeChanges()
-        super.onDestroyView()
-    }
+       /* trackWaterButton.setOnClickListener{
+            viewModel.incrementWaterIntake()
+        }*/
 
-    override fun onWaterIntakeChanged(waterIntake: Int) {
-        //waterCountText.text=waterIntake.toString()
     }
 }
