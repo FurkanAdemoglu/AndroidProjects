1bfb6dc0f066bd7fca701ecc380cbe2e7f95f1c2
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-11-13 13:06:36 +0300

    Add livedata tot the scoreviewmodel

--- GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ GuessTheWordViewModel/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -16,12 +16,14 @@
 
 package com.example.android.guesstheword.screens.score
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -34,6 +36,7 @@ class ScoreFragment : Fragment() {
 
     private lateinit var viewModel: ScoreViewModel
     private lateinit var viewModelFactory: ScoreViewModelFactory
+
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -49,11 +52,14 @@ class ScoreFragment : Fragment() {
                 false
         )
 
-        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
+        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
         viewModel = ViewModelProvider(this, viewModelFactory)
                 .get(ScoreViewModel::class.java)
-
-        binding.scoreText.text = viewModel.score.toString()
+// Add observer for score
+        viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
+            binding.scoreText.text = newScore.toString()
+        })
+       // binding.scoreText.text = viewModel.score.toString()
 
         return binding.root
     }
