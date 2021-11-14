16033fc6f1a00aa6bc717c30f498470018bf1f70
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-11-14 15:11:13 +0300

    Send the order

--- android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/SummaryFragment.kt
+++ android-basics-kotlin-cupcake-app-starter/app/src/main/java/com/example/cupcake/SummaryFragment.kt
@@ -15,6 +15,7 @@
  */
 package com.example.cupcake
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -60,13 +61,37 @@ class SummaryFragment : Fragment() {
      * Submit the order by sharing out the order details to another app via an implicit intent.
      */
     fun sendOrder() {
-        Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
+        val numberOfCupcakes = sharedViewModel.quantity.value ?: 0
+
+        /**
+         * The elvis operator (?:) means that if the expression on the left is not null, then use it. Otherwise if the expression on the left is null, then use the expression to the right of the elvis operator (which is 0 in this case).
+         */
+        val orderSummary = getString(
+            R.string.order_details,
+            sharedViewModel.quantity.value.toString(),
+            sharedViewModel.flavor.value.toString(),
+            sharedViewModel.date.value.toString(),
+            sharedViewModel.price.value.toString()
+        )
+
+        val intent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_SUBJECT, getString(R.string.new_cupcake_order))
+            .putExtra(Intent.EXTRA_TEXT, orderSummary)
+        if (activity?.packageManager?.resolveActivity(intent, 0) != null) {
+            startActivity(intent)
+
+            Toast.makeText(activity, "Send Order", Toast.LENGTH_SHORT).show()
     }
+
+  }
 
     fun cancelOrder() {
         sharedViewModel.resetOrder()
         findNavController().navigate(R.id.action_summaryFragment_to_startFragment)
     }
+
+
 
     /**
      * This fragment lifecycle method is called when the view hierarchy associated with the fragment
